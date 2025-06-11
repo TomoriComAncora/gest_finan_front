d4d4e093437f76e553c688e31fb046d54a0dff56
TomoriComAncora <lucas_fernando@ufms.br>
2025-06-10 23:34:16 -0400

    Refatorar estilos e estrutura do componente Home para melhor organização e responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,38 @@
 import logoImg from "/public/logo.png";
 import Image from "next/image";
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <Image src={logoImg} alt="Logo Gesfin" />
-      </div>
-      <section>
-        <form>
-          <input
-            type="email"
-            required
-            name="email"
-            placeholder="Digite seu email..."
-            className="bg-amber-50"
-          />
-          <input
-            type="password"
-            required
-            name="password"
-            placeholder="********"
-            className="bg-amber-50"
-          />
+      <div className="min-h-screen flex flex-col items-center justify-center bg-primary">
+        <Image src={logoImg} alt="Logo Gesfin" width={230} />
+        <section className="mt-2 flex flex-col items-center justify-center gap-4 w-full max-w-md sm:w-11/12">
+          <form className="flex flex-col items-center justify-center gap-1.5 text-white text-lg pb-4 w-11/12">
+            <input
+              className="h-10 p-4 border-2 border-gray-200 rounded-lg bg-secondary text-lg"
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email..."
+            />
+            <input
+              className="h-10 p-4 border-2 border-gray-200 rounded-lg bg-secondary"
+              type="password"
+              required
+              name="password"
+              placeholder="********"
+            />
 
-          <button type="submit" className="bg-blue-700">Acessar</button>
-        </form>
-        <Link className='text-white bg-button' href={"/signup"} >
-          Não possui cadastro? Cadastre-se
-        </Link>
-      </section>
+            <button type="submit" className="bg-button h-10 text-lg">
+              Acessar
+            </button>
+          </form>
+          <Link className="text-white bg-button" href={"/signup"}>
+            Não possui cadastro? Cadastre-se
+          </Link>
+        </section>
+      </div>
     </>
   );
 }
