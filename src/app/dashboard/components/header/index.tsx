e56f0324a61c902c37769ad2949f3c3b2a7dc479
TomoriComAncora <lucas_fernando@ufms.br>
2025-07-04 17:01:25 -0400

    corrigir link para a página de transações no cabeçalho

--- src/app/dashboard/components/header/index.tsx
+++ src/app/dashboard/components/header/index.tsx
@@ -29,7 +29,7 @@ export function Header() {
 
         <nav className="flex items-center gap-3.5 text-white">
           <Link
-            href={"/dashboard/myTransaction"}
+            href={"/dashboard/mytransaction"}
             className="transition-all duration-500 hover:text-button"
           >
             Minhas Transações
